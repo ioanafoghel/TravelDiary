8dc9571b37ecd51ede5276c9832a35816a797325
ioanafoghel <ioanafoghel@gmail.com>
2016-04-19 23:21:52 +0200

    JSON file added

--- app/src/main/java/com/example/ioana/traveldiaryapp/activities/MainActivity.java
+++ app/src/main/java/com/example/ioana/traveldiaryapp/activities/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         btnTrips.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), TravelNoteActivity.class);
+                Intent intent = new Intent(v.getContext(), TripsActivity.class);
                 startActivity(intent);
             }
         });
